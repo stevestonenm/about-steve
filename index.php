e3ac1abcd43eff598725dc7ffd4bd91c1c82f9bd
Steve Stone <stoneflynm@gmail.com>
2017-07-10 16:15:38 -0600

    added index file and added images

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>This is About Steve</title>
+	</head>
+	<body>
+		<h1>Steve Stone</h1>
+		<p>I am southwest through and through.  Born in Albuquerque on February 26, 1979.
+			I have spent most of my life living in New Mexico but I have lived in a few other states.
+			These include Texas, Colorado, Montana and Alaska.</p>
+		<p>One of my goals I am trying to achieve is to complete an ultra-marathon.  If you are not familiar with
+			an ultra-marathon it is any distance that is longer than 26 miles.  I started on this goal last year
+			but I injured my hip before I was able to reach my goal</p>
+		<p><img src="images/steve-run.jpg" alt="finish line"></p>
+	</body>
+</html>
